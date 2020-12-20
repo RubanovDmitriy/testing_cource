9624b0278c61e54e85c45a51e5459cb1ea9e1e80
RubanovDmitriy <dmitriyrubanov1988@gmail.com>
2020-12-20 19:12:28 +0700

    API testing of https://dog.ceo/

--- 03_api_testing/test_gogs_api.py
+++ 03_api_testing/test_gogs_api.py
@@ -1,14 +1,27 @@
+import pytest
 import requests
+import validators
 from jsonschema import validate
+from urllib.parse import urljoin
+
+DOMAIN = 'https://dog.ceo/api/'
 
 
-def test_url_status():
-    r = requests.get('https://dog.ceo/api/breed/hound/images')
-    assert r.status_code == 200
+@pytest.mark.parametrize('url, status_code',
+                         [
+                             ('breed/hound/images', 200),
+                             ('breeds/list/all', 200),
+                             ('breeds/image/random', 200),
+                             ('breed/hound/images', 200),
+                             ('breed/hound/list', 200),
+                         ])
+def test_url_status(url, status_code):
+    res = requests.get(urljoin(DOMAIN, url))
+    assert res.status_code == status_code
 
 
 def test_api_json_schema():
-    res = requests.get('https://dog.ceo/api/breed/hound/images')
+    res = requests.get(urljoin(DOMAIN, 'breed/hound/images'))
 
     schema = {
         "type": "object",
@@ -20,5 +33,39 @@ def test_api_json_schema():
 
     validate(instance=res.json(), schema=schema)
 
-def test_hound_afghan():
-    pass
+
+@pytest.mark.parametrize('url, number',
+                         [
+                             ('breed/hound/images/random/1', 1),
+                             ('breed/hound/images/random/5', 5),
+                             ('breed/hound/images/random/10', 10),
+                             ('breed/hound/images/random/33', 33),
+                             ('breed/hound/images/random/999', 999),
+                         ])
+def test_len_random_imgages(url, number):
+    res = requests.get(urljoin(DOMAIN, url))
+    images_list = res.json()["message"]
+    assert len(images_list) == number
+
+
+def test_images_are_urls():
+    res = requests.get(urljoin(DOMAIN, 'breeds/image/random'))
+    assert validators.url(res.json()["message"])
+
+
+def test_encoding():
+    res = requests.get(urljoin(DOMAIN, 'breeds/list/all'))
+    assert res.encoding == 'utf-8'
+
+
+@pytest.mark.parametrize('header, header_status',
+                         [
+                             ('content-type', 'application/json'),
+                             ('Content-Encoding', 'gzip'),
+                             ('Server', 'cloudflare'),
+                             ('Cache-Control', 'max-age=1800'),
+                         ])
+def test_headers(header, header_status):
+    res = requests.get(urljoin(DOMAIN, 'breeds/list/all'))
+    assert res.headers[header] == header_status
+
