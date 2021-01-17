45f2f457e3d972a37de83a97df66170defe1ea13
RubanovDmitriy <dmitriyrubanov1988@gmail.com>
2021-01-17 21:30:59 +0700

    dog res parsing

--- 03_api_testing/dogs_res_parsing.py
+++ 03_api_testing/dogs_res_parsing.py
@@ -1,37 +1,33 @@
+import validators
 import requests
 import re
 
 # Про передачу параметров
-r = requests.get('https://dog.ceo/api/breed/hound/images')
+r = requests.get('https://api.openbrewerydb.org/breweries?per_page=1')
 
-# print("\n------- status/headers/encoding ---------")
-#
-# print(r.status_code)
-# print(r.headers['content-type'])
-# print(r.encoding)
-#
-# print("\n----------------- text ------------------")
-# print(r.text)
-# print("\n----------------- json ------------------")
-#
-# print(r.json())
+print("\n------- status/headers/encoding ---------")
+
+print(r.status_code)
+print('content-type =>', r.headers['content-type'])
+print('Content-Encoding =>', r.headers['Content-Encoding'])
+print('Server =>', r.headers['Server'])
+print('Cache-Control =>', r.headers['Cache-Control'])
+print(r.encoding)
+
+print("\n----------------- text ------------------")
+print(r.text)
+print("\n----------------- json ------------------")
+
+print(r.json())
+print('len = ', len(r.json()))
+# print(r.json().get('message'))
 # print(type(r.json().get('message')))
-#
-# print("\n---------------- headers ----------------")
-#
-# for key, value in r.headers.items():
-#     print(key, ' => ', value)
+# print(validators.url(r.json().get('message')))
 
-prog = re.compile(r'^https://images.dog.ceo/breeds/hound-afghan.*')
+print("\n---------------- headers ----------------")
 
-links_list = r.json().get("message")
-print(links_list)
-for link in links_list:
-    x = prog.match(link)
-    if x is None:
-        continue
-    print(x.group(0))
-    # if link.startswith('https://images.dog.ceo/breeds/hound-afghan/'):
-    #     print(link)
+print(r.headers)
+for key, value in r.headers.items():
+    print(key, ' => ', value)
 
 
